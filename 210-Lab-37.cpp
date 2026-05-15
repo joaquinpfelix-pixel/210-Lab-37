6af2fe47aa9044066fbe78d3851f65faddd898df
JoaquinF <joaquinpfelix@gmail.com>
2026-05-14 20:15:52 -0700

    Milestone 3

--- 210-Lab-37.cpp
+++ 210-Lab-37.cpp
@@ -3,13 +3,20 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <list>
 
 using namespace std;
 
 // Constants
 const string DATA_FILE = "lab-37-data-3.txt";
+const int HASH_SIZE = 1000;
+const int DISPLAY_LIMIT = 10;
 
-int sum_ascii(const string& text);
+// gen_hash_index() calculates the hash index for the string
+// arguments: a string
+// returns: interger hash index
+int gen_hash_index(const string& text);
 
 int main() {
     ifstream fin(DATA_FILE);
@@ -20,35 +27,60 @@ int main() {
              << DATA_FILE
              << endl;
         
-        return 0;
+        return 1;
     }
 
+    // Hash table;
+    // key  = has index
+    // value = list of codes
+    map<int, list<string>> hash_table;
 
     string code;
-    long long grand_total = 0;
 
+
+    // Read file data
     while (fin >> code)
     {
-        grand_total += sum_ascii(code);
+        int hash_index = gen_hash_index(code);
+
+        // Insert code into list at that hash index
+        hash_table[hash_index].push_back(code);
     }
 
     fin.close();
 
-    cout << "Grand total: "
-         << grand_total
-         << endl;
+    int displayed = 0;
+
+    for (auto pair : hash_table)
+    {
+        cout << "Hash Index: "
+             << pair.first
+             << endl;
+        cout << "Codes: ";
+
+        for (string code_value : pair.second)
+        {
+            cout << code_value << " ";
+        }
+        cout << endl << endl;
+
+        displayed++;
+
+        if (displayed >= DISPLAY_LIMIT)
+        {
+            break;
+        }
+    }
     
 
     return 0;
 }
 
 
-// sum_ascii() calculates the sum of the ASCII values of the string
+// gen_hash_index() calculates the hash index for the string
 // arguments: a string
-// returns: interger ASCII total
-
-
-int sum_ascii(const string& text)
+// returns: interger hash index
+int gen_hash_index(const string& text)
 {
     int total = 0;
 
